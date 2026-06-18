8e15ffa0d94b31326afc826b95b36c4ba6f7de88
Saenzamirxd <saenzvelizamirimanol@gmail.com>
2026-06-18 10:10:12 -0500

    SE AGREGA UNA FUNCIOON PARA CALCULAR OPERACIONES

--- PL_6/utils.py
+++ PL_6/utils.py
@@ -9,3 +9,17 @@ def mostrar_bienvenida():
         None
     """
     print("\n¡Bienvenido al programa de funciones!")
+
+    #Amir Sáenz
+    def calcular_operaciones(numero1, numero2):
+        """
+         Calcula el promedio de dos números.
+        Parámetros:
+        numero1 (float): Primer número.
+        numero2 (float): Segundo número.
+        Retorna:
+        float: Promedio de los números.
+    """
+    suma = numero1 + numero2
+    promedio = suma/2
+    return suma, promedio
